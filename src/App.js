851cab70f0d40a0990dc15c0eb6db859b0f7afd7
Vishal <vibrus77@gmail.com>
2024-05-19 14:05:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      	<Routes>
+			<Route exact path="/" element={<Home />} />
+			<Route path='/login' element={<Login />} />
+      	</Routes>
+    </Router>
+  );
+}
+
+export default App;
